a886ae914f7db74ea7818ad73eabed15e926d7f4
Derrick Parkhurst <derrickparkhurst@derrick-parkhursts-macbook-pro.local>
2010-04-01 18:56:25 -0500

    speaker updates

--- db/fixtures/presentations.rb
+++ db/fixtures/presentations.rb
@@ -134,4 +134,84 @@ Presentation.seed(:title, :author) do |s|
   s.video=""
 end
 
+Presentation.seed(:title, :author) do |s|  
+  s.title = "Title TBD"   
+  s.author = "Stephen Nolen"
+  s.affiliation = "CIO, Shawnee OK"
+  s.email = "SNolen@shawneeok.org"  
+  s.biography = "Stephen Nolen is the Chief Information Officer for the City of Shawnee Oklahoma. He has been with Shawnee since January 2000 and previously server as the IT Director for the Citizen Potawatomi Nation and the Data Processing Director / Assistant to City Manager for the City of Tecumseh. Mr. Nolen has been involved in municipal or tribal government since 1991 and has worked in the Information Technology field since the early 80’s"
+  s.photo = "/images/bio_photo_stephennolen.jpg"
+  s.theme = ""
+  s.abstract = ""
+  s.comments = ""
+  s.format = "talk"
+  s.agreed_to_terms = "yes"
+  s.status = "approved"
+  s.start_time = Time.local(2010,"may",6,19,0,0)  
+  s.duration = 30
+  s.slides=""
+  s.video=""
+end
+
+Presentation.seed(:title, :author) do |s|  
+  s.title = "Title TBD"   
+  s.author = "Scott Anderson"
+  s.affiliation = "CIO / Interim Finance Director"
+  s.email = "sanderson@spigit.com"  
+  s.biography = "Scott is the Vice President of Partnerships and Channels at Spigit, a software company focused on innovation in government and commercial accounts. Scott has been working with resellers and integrators in the government space, and with  government officials at the city, county, and federal level.  Prior to joining Spigit Scott spent 16 years at Sun Microsystems, where he was a Director of the ISV  group. Scott has an MBA from John F. Kennedy University and B.A. from the University of California, Berkeley."
+  s.photo = "/images/bio_photo_scottanderson.jpg"
+  s.theme = ""
+  s.abstract = ""
+  s.comments = ""
+  s.format = "talk"
+  s.agreed_to_terms = "yes"
+  s.status = "approved"
+  s.start_time = Time.local(2010,"may",6,19,0,0)  
+  s.duration = 30
+  s.slides=""
+  s.video=""
+end
+
+Presentation.seed(:title, :author) do |s|  
+  s.title = "Title TBD"   
+  s.author = "Mike Koehler"
+  s.affiliation = "Schnake Turnbo Frank | PR"
+  s.email = "mkoehler@stfpr.com"  
+  s.biography = "Mike Koehler is the media director for Schnake Turnbo Frank | PR. In this role, he counsels the firm and its clients on the best use of social media for each campaign or project. He has more than 15 years of journalism experience, which includes serving as the multimedia editor and deputy sports editor of The Oklahoman, a statewide newspaper. Koehler has a vast knowledge of different social media outlets. He has more than 1,800 followers on Twitter and consistently ranks among the top 20 in Oklahoma City for the most followers."
+  s.photo = "/images/bio_photo_mikekoehler.jpg"
+  s.theme = ""
+  s.abstract = ""
+  s.comments = ""
+  s.format = "talk"
+  s.agreed_to_terms = "yes"
+  s.status = "approved"
+  s.start_time = Time.local(2010,"may",6,19,0,0)  
+  s.duration = 30
+  s.slides=""
+  s.video=""
+end
+
+Presentation.seed(:title, :author) do |s|  
+  s.title = "Title TBD"   
+  s.author = "Alex Pettit"
+  s.affiliation = "CIO Oklahoma"
+  s.email = "alex@organizationalsurvival.com"  
+  s.biography = "<p>Alex joined Brown University in 2009 as an independent consultant responsible for project management of the planning, coordinating, and execution of the rapid development of a fully functional and tested disaster recovery plan and the coordination of the renovation of Brown University’s sole data center.  Prior to that, Alex worked for Marsh McLennan in the areas of BCP strategy & vision, disaster recovery, & business resumption planning, focusing on the proactive preparation of people to effectively survive through and recover from a major crisis event.  For ten years Alex has been actively involved in continuity of operations planning, crisis leadership, emergency operations center design, civil defense planning in Texas, and has personally managed and supported teams through major crisis events. </p>
+<p>Alex has global industry, government and Big 4 consulting experience, and is a recognized leader in Enterprise Architecture.  He has directed both industry and consulting organizations in diverse roles as a Chief Technology Officer, Vice President and BCP Service Line Leader, Senior Manager, and Regional Director of Technology and Telecommunications.  Alex has received the Best of Texas Award for IT Leadership, the Public Technology Institute IT Leadership Award, and the Society for Information Management IT Executive of the Year Award for his leadership. </p>
+<p>In addition, Alex is a published author on IT Leadership in Studies in E-Government, Government Technology Magazine, Texas Technology Magazine, the Journal for Enterprise Architecture, and most recently the SIM Guide to Enterprise Architecture (2009).</p>
+<p>Alex completed undergraduate coursework at the University of Wisconsin at Parkside and received an MBA from Loyola University of Chicago.  He is currently working toward completion of a PhD in Interdisciplinary Information Sciences at the University of North Texas. He is a member of the Beta Gamma Sigma International Honor Society and teaches classes at the University of Phoenix in the areas of Technology Survival Planning, Enterprise Architecture, and E-Business.  He is certified by the Disaster Recovery Institute International as a Certified Business Continuity Professional (CBCP). In addition, he holds certifications with Novell, Filenet, Sun Systems, and IBM.</p> 
+<p>Alex holds professional membership in the Disaster Recovery Institute International, the International City Managers Association, the Texas City Managers Association, the Society for Information Management, the Society of Information Managers, and the American Institute of Technology Professionals.</p>"
+  s.photo = "/images/bio_photo_alexpettit.jpg"
+  s.theme = ""
+  s.abstract = ""
+  s.comments = ""
+  s.format = "talk"
+  s.agreed_to_terms = "yes"
+  s.status = "approved"
+  s.start_time = Time.local(2010,"may",6,19,0,0)  
+  s.duration = 30
+  s.slides=""
+  s.video=""
+end
+
 
