2cf98b5264f5246272bd11d4eda4a17e3ef728ba
Derrick Parkhurst <derrickparkhurst@derrick-parkhursts-macbook-pro.local>
2010-04-21 18:03:47 -0500

    bio photo update

--- db/fixtures/items.rb
+++ db/fixtures/items.rb
@@ -18,8 +18,8 @@ valid May 6th and 7th, 2010
 <br></p>
 "
   s.current_price = 49
-  s.quantity_original = 200
-  s.quantity_available = 200
+  s.quantity_original = 100
+  s.quantity_available = 100
   s.scheduled_time = Time.local(2010,"may",6,9,0,0)  
 end
 
@@ -36,8 +36,8 @@ valid 5pm-9pm, May 6th, 2010
 <br></p>
 "
   s.current_price = 69
-  s.quantity_original = 200
-  s.quantity_available = 200
+  s.quantity_original = 100
+  s.quantity_available = 100
   s.scheduled_time = Time.local(2010,"may",6,18,0,0)  
 end
 
@@ -57,7 +57,7 @@ valid May 6th and 7pm, 2010
 <br></p>
 "
   s.current_price = 99
-  s.quantity_original = 200
-  s.quantity_available = 200
+  s.quantity_original = 100
+  s.quantity_available = 100
   s.scheduled_time = Time.local(2010,"may",6,9,0,0)  
 end
